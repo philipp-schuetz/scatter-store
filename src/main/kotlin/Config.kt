e16c4c44381f15398e474ea0df07a54784547f1d
philipp-schuetz <mail@philippschuetz.com>
2024-03-10 22:06:22 +0100

    Add Config.kt

---
+++ src/main/kotlin/Config.kt
@@ -0,0 +1,15 @@
+package com.philippschuetz
+
+import java.net.URL
+
+data class ConfigSectionFile(val id: String, val providers: List<ProviderType>, val encryption: String, val name: String)
+
+data class ConfigSectionProvider(val name: ProviderType, val username: String, val password: String)
+data class ConfigSectionProviderFTP(val name: String = "FTP", val username: String, val password: String, val url: URL, val port: Int)
+
+data class ConfigSectionEncryption(val id: String, val algorithm: EncryptionType, val key: String)
+data class Config(
+    val files: List<ConfigSectionFile>,
+    val providers: List<ConfigSectionProviderFTP>,
+    val encryption: List<ConfigSectionEncryption>
+)
