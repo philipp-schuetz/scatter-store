10df37247be22b8485f051476260addf71968f34
philipp-schuetz <mail@philippschuetz.com>
2024-03-10 22:05:58 +0100

    Add dependencies for cli and ftp

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,8 @@ dependencies {
 
     implementation("com.sksamuel.hoplite:hoplite-core:2.8.0.RC3")
     implementation("com.sksamuel.hoplite:hoplite-json:2.8.0.RC3")
+    implementation("info.picocli:picocli:4.7.5")
+    implementation("com.jcraft:jsch:0.1.55")
 }
 
 tasks.test {
