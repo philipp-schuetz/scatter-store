18bbdc1b9093177cf503e55410d95a8b81395832
philipp-schuetz <mail@philippschuetz.com>
2024-03-11 13:33:40 +0100

    Add dependencies for sqlite db

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,9 @@ dependencies {
     implementation("com.sksamuel.hoplite:hoplite-json:2.8.0.RC3")
     implementation("info.picocli:picocli:4.7.5")
     implementation("com.jcraft:jsch:0.1.55")
+    implementation("org.ktorm:ktorm-core:3.6.0")
+    implementation("org.ktorm:ktorm-support-sqlite:3.6.0")
+    implementation("org.xerial:sqlite-jdbc:3.45.1.0")
 }
 
 tasks.test {
