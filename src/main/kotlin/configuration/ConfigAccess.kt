8a829137e68f7556fe6d96a4d558851d7e92fefd
philipp-schuetz <mail@philippschuetz.com>
2024-03-14 18:51:52 +0100

    Document ConfigAccess.kt

--- src/main/kotlin/configuration/ConfigAccess.kt
+++ src/main/kotlin/configuration/ConfigAccess.kt
@@ -32,6 +32,7 @@ private data class ConfigModel(
 /**
  * Load the config and return the serialized version.
  * @param reload If false, only the cached version of the config is returned. If true, the config is reloaded from the file before returning.
+ * @return The loaded configuration model.
  */
 private fun readConfig(reload: Boolean = false): ConfigModel {
     if (reload || config == null)
@@ -50,10 +51,22 @@ private fun writeConfig(newContent: ConfigModel) {
 
 private var config: ConfigModel? = null
 
+/**
+ * Check if a given id exists in a list.
+ * @param list The list to check.
+ * @param id The id to look for.
+ * @param idExtractor A function to extract the id from an item in the list.
+ * @return True if the id exists in the list, false otherwise.
+ */
 private fun <T> idInList(list: List<T>, id: String, idExtractor: (T) -> String): Boolean {
     return list.any { idExtractor(it) == id }
 }
 
+/**
+ * Add a new encryption key to the configuration file.
+ * @param algorithm The encryption algorithm associated with the key.
+ * @param key The encryption key.
+ */
 fun addEncryptionKey(algorithm: EncryptionType, key: String) {
     val modifiedConfig = readConfig()
     var generatedId: String
@@ -66,6 +79,12 @@ fun addEncryptionKey(algorithm: EncryptionType, key: String) {
     writeConfig(modifiedConfig)
 }
 
+/**
+ * Retrieve an encryption key from the configuration file.
+ * @param keyIndex The index of the key in the configuration file.
+ * @param algorithm The encryption algorithm associated with the key.
+ * @return The encryption key.
+ */
 fun getEncryptionKey(keyIndex: Int, algorithm: EncryptionType): String {
     val modifiedConfig = readConfig().encryption[keyIndex]
     if (modifiedConfig.algorithm != algorithm) {
