aaa30e80080fd440eedd7c79ba630158db28a2a1
philipp-schuetz <mail@philippschuetz.com>
2024-03-15 18:04:03 +0100

    Add getFileByFileName in DB

--- src/main/kotlin/configuration/DB.kt
+++ src/main/kotlin/configuration/DB.kt
@@ -1,14 +1,19 @@
 package com.philippschuetz.configuration
 
-import com.philippschuetz.getDataFolder
+import com.philippschuetz.getDBPath
 import org.ktorm.database.Database
 import org.ktorm.dsl.*
+import org.ktorm.logging.ConsoleLogger
+import org.ktorm.logging.LogLevel
 import org.ktorm.schema.*
 
 data class FileTypeDB(val fileId: String, val name: String, val providerIdList: List<String>, val encryptionId: String)
 
 class DB {
-    private val database: Database = Database.connect("jdbc:sqlite:${getDataFolder()}/scatter-store.sqlite")
+    private val database: Database = Database.connect(
+        url = "jdbc:sqlite:${getDBPath()}",
+        logger = ConsoleLogger(LogLevel.ERROR)
+    )
 
     object FilesTable : Table<Nothing>("t_file") {
         val id = int("id").primaryKey()
@@ -18,6 +23,9 @@ class DB {
         val encryptionId = varchar("encryption_id")
     }
 
+    /**
+     * Create an empty file table in the database.
+     */
     fun init() {
         database.useConnection { conn ->
             val sql = """
@@ -36,6 +44,10 @@ class DB {
         }
     }
 
+    /**
+     * Add a file to the database.
+     * @param file File to add.
+     */
     fun addFile(file: FileTypeDB) {
         database.insert(FilesTable) {
             set(it.fileId, file.fileId)
@@ -45,6 +57,10 @@ class DB {
         }
     }
 
+    /**
+     * Get information on a file using its unique identifier.
+     * @param fileId Unique file identifier.
+     */
     fun getFileByFileId(fileId: String): FileTypeDB {
         val response = database
             .from(FilesTable)
@@ -62,15 +78,24 @@ class DB {
         return response.first()
     }
 
-    fun getAllFiles() {
-        for (row in database.from(FilesTable).select()) {
-            println(row)
-        }
+    /**
+     * Get information on a file by searching for its original name.
+     * @param fileName Original file name.
+     */
+    fun getFileByFileName(fileName: String): List<FileTypeDB> {
+        val response = database
+            .from(FilesTable)
+            .select()
+            .where { (FilesTable.name like "%${fileName.lowercase()}%") }
+            .map { row ->
+                FileTypeDB(
+                    fileId = row[FilesTable.fileId]!!,
+                    name = row[FilesTable.name]!!,
+                    providerIdList = row[FilesTable.providerIdList]!!.split(","),
+                    encryptionId = row[FilesTable.encryptionId]!!
+                )
+            }
+
+        return response
     }
-
-}
-
-
-fun main() {
-    DB().init()
 }
