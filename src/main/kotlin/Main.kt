335330b89eeaf069ed8dd6dd38a8c927dea7080a
philipp-schuetz <mail@philippschuetz.com>
2024-03-16 15:40:13 +0100

    Add --init; Work on --upload

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,14 @@
 package com.philippschuetz
 
+import com.philippschuetz.configuration.DB
+import com.philippschuetz.configuration.initConfig
+import com.philippschuetz.encryption.EncryptionAES
 import picocli.CommandLine
+import java.nio.file.Path
 import java.util.concurrent.Callable
+import kotlin.io.path.createFile
+import kotlin.io.path.isDirectory
+import kotlin.io.path.isRegularFile
 import kotlin.system.exitProcess
 
 
@@ -11,17 +18,28 @@ import kotlin.system.exitProcess
 )
 class ScatterStore : Callable<Int> {
 
-    @CommandLine.Option(names = ["-f", "--file"], description = ["RegEx for files to upload."])
-    lateinit var file: Regex
+    @CommandLine.Option(names = ["--init"], description = ["Create config and Database."])
+    private var initialize: Boolean = false
 
-    @CommandLine.Option(names = ["-k", "--key"], description = ["Key for file encryption, see config. default: 0"])
-    var algorithm: Int = 0
-
-    @CommandLine.Option(names = ["-p", "--provider"], description = ["Add a cloud storage provider."])
-    lateinit var provider: ProviderType
-
+    @CommandLine.Option(names = ["--upload"], description = ["Upload a file."])
+    private lateinit var file: Path
 
     override fun call(): Int {
+        if (initialize) {
+            // create an empty config file with an AES Key, create a database file and add db structure
+            initConfig()
+            EncryptionAES().generateKey()
+            getDBPath().createFile()
+            DB().init()
+        } else if (this::file.isInitialized) {
+            val files: MutableList<Path> = mutableListOf()
+            if (file.isDirectory()) {
+                TODO("scan directory for files recursively")
+            } else if (file.isRegularFile()) {
+                files.add(file)
+            } else
+                return 1
+        }
         return 0
     }
 }
