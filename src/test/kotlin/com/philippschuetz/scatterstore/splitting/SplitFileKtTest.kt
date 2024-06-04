cbe56e4a9544bd1b1941bec0ca9a977aa574a7b1
philipp-schuetz <mail@philippschuetz.com>
2024-06-04 20:38:37 +0200

    Update SplitFileKtTest.kt

--- src/test/kotlin/com/philippschuetz/scatterstore/splitting/SplitFileKtTest.kt
+++ src/test/kotlin/com/philippschuetz/scatterstore/splitting/SplitFileKtTest.kt
@@ -1,26 +1,44 @@
 package com.philippschuetz.scatterstore.splitting
 
+import com.philippschuetz.scatterstore.areFilesIdentical
 import org.junit.jupiter.api.AfterEach
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import java.util.*
 import kotlin.io.path.Path
+import kotlin.io.path.deleteExisting
+import kotlin.io.path.exists
 
 class SplitFileKtTest {
     private val splittingResourcesBasePath = "src/test/resources/splitting/"
-
-    @AfterEach
-    fun tearDown() {
-    }
+    private val fileShards = listOf(
+        Path(splittingResourcesBasePath + "shard-1"),
+        Path(splittingResourcesBasePath + "shard-2")
+    )
+    private val fileShardsTmp = listOf(
+        Path(splittingResourcesBasePath + "shard-tmp-1"),
+        Path(splittingResourcesBasePath + "shard-tmp-2")
+    )
 
     @Test
     fun splitFile() {
         splitFile(
             Path(splittingResourcesBasePath + "to-split.txt"),
             2,
-            UUID.randomUUID().toString(),
+            "shard-tmp",
             Path(splittingResourcesBasePath)
         )
+        for (i in fileShards.indices) {
+            assertTrue(areFilesIdentical(fileShards[i], fileShardsTmp[i]))
+        }
+    }
+
+    @AfterEach
+    fun tearDown() {
+        for (encryptedFile in fileShards) {
+            if (encryptedFile.exists()) {
+                encryptedFile.deleteExisting()
+            }
+        }
     }
 }
