2b3518c0c4baa0fd173fc287e21549ba20a81114
philipp-schuetz <mail@philippschuetz.com>
2024-06-10 20:26:07 +0200

    Update Navbar.svelte

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,21 +1,24 @@
 <script>
     import {AdjustmentsVerticalSolid, ShareNodesSolid, InfoCircleSolid, ServerSolid} from "flowbite-svelte-icons";
     import {BottomNav, BottomNavItem} from "flowbite-svelte";
+    import { page } from '$app/stores';
+    $: activeUrl = $page.url.pathname;
+    let iconClass = "w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"
 </script>
 
 <nav>
-    <BottomNav position="absolute" classInner="grid-cols-4">
-        <BottomNavItem btnName="Shares">
-            <ShareNodesSolid class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500" />
+    <BottomNav {activeUrl} position="absolute" classInner="grid-cols-4">
+        <BottomNavItem btnName="Shares" href="/shares">
+            <ShareNodesSolid class={iconClass} />
         </BottomNavItem>
-        <BottomNavItem btnName="Providers">
-            <ServerSolid class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500" />
+        <BottomNavItem btnName="Providers" href="/providers">
+            <ServerSolid class={iconClass}/>
         </BottomNavItem>
-        <BottomNavItem btnName="Settings">
-            <AdjustmentsVerticalSolid class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500" />
+        <BottomNavItem btnName="Settings" href="/settings">
+            <AdjustmentsVerticalSolid class={iconClass}/>
         </BottomNavItem>
-        <BottomNavItem btnName="About">
-            <InfoCircleSolid class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500" />
+        <BottomNavItem btnName="About" href="/about">
+            <InfoCircleSolid class={iconClass}/>
         </BottomNavItem>
     </BottomNav>
 </nav>
