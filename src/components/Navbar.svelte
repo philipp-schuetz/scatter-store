51118fcd1d499016f9468c0c150f5b623da4d3c4
philipp-schuetz <mail@philippschuetz.com>
2024-06-10 19:36:38 +0200

    Add Navbar.svelte

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,21 @@
+<script>
+    import {AdjustmentsVerticalSolid, ShareNodesSolid, InfoCircleSolid, ServerSolid} from "flowbite-svelte-icons";
+    import {BottomNav, BottomNavItem} from "flowbite-svelte";
+</script>
+
+<nav>
+    <BottomNav position="absolute" classInner="grid-cols-4">
+        <BottomNavItem btnName="Shares">
+            <ShareNodesSolid class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500" />
+        </BottomNavItem>
+        <BottomNavItem btnName="Providers">
+            <ServerSolid class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500" />
+        </BottomNavItem>
+        <BottomNavItem btnName="Settings">
+            <AdjustmentsVerticalSolid class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500" />
+        </BottomNavItem>
+        <BottomNavItem btnName="About">
+            <InfoCircleSolid class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500" />
+        </BottomNavItem>
+    </BottomNav>
+</nav>
