654224167b70bb7e74e7d09a2f6e174088dad53a
philipp-schuetz <mail@philippschuetz.com>
2024-06-10 20:25:52 +0200

    Add shares page and set as default

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/tauri";
+  import { goto } from '$app/navigation';
+  import { onMount } from 'svelte';
 
   let name = "";
   let greetMsg = "";
+
+  onMount(() => {
+      goto('/shares');
+  });
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
