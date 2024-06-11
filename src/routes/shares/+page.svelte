1af2c4f8b9b991d701a2aaad728237c53804b86a
philipp-schuetz <mail@philippschuetz.com>
2024-06-11 20:39:29 +0200

    Add table to display current shares

--- src/routes/shares/+page.svelte
+++ src/routes/shares/+page.svelte
@@ -1,6 +1,52 @@
-<script lang="ts">
+<script>
+    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, ButtonGroup, Button } from 'flowbite-svelte';
+    import { CloudArrowUpSolid, EditSolid } from 'flowbite-svelte-icons';
+
 </script>
 
-<div class="container">
-    Shares
-</div>
+<Table striped={true} shadow>
+    <TableHead>
+        <TableHeadCell>Name</TableHeadCell>
+        <TableHeadCell>Path</TableHeadCell>
+        <TableHeadCell>Last Uploaded</TableHeadCell>
+        <TableHeadCell>
+            <span class="sr-only">Actions</span>
+        </TableHeadCell>
+    </TableHead>
+    <TableBody tableBodyClass="divide-y">
+        <TableBodyRow>
+            <TableBodyCell>Documents</TableBodyCell>
+            <TableBodyCell>/home</TableBodyCell>
+            <TableBodyCell>01/01/2001 00:00</TableBodyCell>
+            <TableBodyCell>
+                <ButtonGroup>
+                    <Button href="/">
+                        <CloudArrowUpSolid class="w-4 h-4 me-2" />
+                        Upload
+                    </Button>
+                    <Button href="/">
+                        <EditSolid class="w-4 h-4 me-2" />
+                        Edit
+                    </Button>
+                </ButtonGroup>
+            </TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell>Documents</TableBodyCell>
+            <TableBodyCell>/home</TableBodyCell>
+            <TableBodyCell>01/01/2001 00:00</TableBodyCell>
+            <TableBodyCell>
+                <ButtonGroup>
+                    <Button href="/">
+                        <CloudArrowUpSolid class="w-4 h-4 me-2" />
+                        Upload
+                    </Button>
+                    <Button href="/">
+                        <EditSolid class="w-4 h-4 me-2" />
+                        Edit
+                    </Button>
+                </ButtonGroup>
+            </TableBodyCell>
+        </TableBodyRow>
+    </TableBody>
+</Table>
