4c84e677c6a1a214f9def75c7c51bc34896da60a
philipp-schuetz <mail@philippschuetz.com>
2024-06-11 21:58:26 +0200

    Update shares page

--- src/routes/shares/+page.svelte
+++ src/routes/shares/+page.svelte
@@ -1,6 +1,15 @@
 <script>
-    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, ButtonGroup, Button } from 'flowbite-svelte';
-    import { CloudArrowUpSolid, EditSolid } from 'flowbite-svelte-icons';
+    import {
+        Table,
+        TableBody,
+        TableBodyCell,
+        TableBodyRow,
+        TableHead,
+        TableHeadCell,
+        ButtonGroup,
+        Button
+    } from 'flowbite-svelte';
+    import {CloudArrowUpSolid, EditSolid, CirclePlusSolid} from 'flowbite-svelte-icons';
 
 </script>
 
@@ -21,11 +30,11 @@
             <TableBodyCell>
                 <ButtonGroup>
                     <Button href="/">
-                        <CloudArrowUpSolid class="w-4 h-4 me-2" />
+                        <CloudArrowUpSolid class="w-4 h-4 me-2"/>
                         Upload
                     </Button>
                     <Button href="/">
-                        <EditSolid class="w-4 h-4 me-2" />
+                        <EditSolid class="w-4 h-4 me-2"/>
                         Edit
                     </Button>
                 </ButtonGroup>
@@ -38,12 +47,25 @@
             <TableBodyCell>
                 <ButtonGroup>
                     <Button href="/">
-                        <CloudArrowUpSolid class="w-4 h-4 me-2" />
+                        <CloudArrowUpSolid class="w-4 h-4 me-2"/>
                         Upload
                     </Button>
                     <Button href="/">
-                        <EditSolid class="w-4 h-4 me-2" />
+                        <EditSolid class="w-4 h-4 me-2"/>
                         Edit
+                    </Button>
+                </ButtonGroup>
+            </TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+            <TableBodyCell></TableBodyCell>
+            <TableBodyCell></TableBodyCell>
+            <TableBodyCell></TableBodyCell>
+            <TableBodyCell>
+                <ButtonGroup>
+                    <Button href="/">
+                        <CirclePlusSolid class="w-4 h-4 me-2"/>
+                        Create a share
                     </Button>
                 </ButtonGroup>
             </TableBodyCell>
