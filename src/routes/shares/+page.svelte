2689afd132491c8a3ecd92e7e7269866e6bc9e3c
philipp-schuetz <mail@philippschuetz.com>
2024-06-12 17:22:44 +0200

    Fill shares table with dynamic data

--- src/routes/shares/+page.svelte
+++ src/routes/shares/+page.svelte
@@ -11,6 +11,27 @@
     } from 'flowbite-svelte';
     import {CloudArrowUpSolid, EditSolid, CirclePlusSolid} from 'flowbite-svelte-icons';
 
+    const shareData = [
+        {
+            id: "1234ger34g43QW",
+            name: "Documents",
+            path: "/documents",
+            lastUploaded: "01/01/2000 00:00"
+        },
+        {
+            id: "fga4a4as4ga4a4g",
+            name: "Photos",
+            path: "/photos",
+            lastUploaded: "01/01/2001 00:01"
+        },
+        {
+            id: "a4gg44g4g8989",
+            name: "Music",
+            path: "/music",
+            lastUploaded: "01/01/2002 00:02"
+        }
+    ]
+
 </script>
 
 <Table striped={true} shadow>
@@ -23,40 +44,25 @@
         </TableHeadCell>
     </TableHead>
     <TableBody tableBodyClass="divide-y">
-        <TableBodyRow>
-            <TableBodyCell>Documents</TableBodyCell>
-            <TableBodyCell>/home</TableBodyCell>
-            <TableBodyCell>01/01/2001 00:00</TableBodyCell>
-            <TableBodyCell>
-                <ButtonGroup>
-                    <Button href="/">
-                        <CloudArrowUpSolid class="w-4 h-4 me-2"/>
-                        Upload
-                    </Button>
-                    <Button href="/">
-                        <EditSolid class="w-4 h-4 me-2"/>
-                        Edit
-                    </Button>
-                </ButtonGroup>
-            </TableBodyCell>
-        </TableBodyRow>
-        <TableBodyRow>
-            <TableBodyCell>Documents</TableBodyCell>
-            <TableBodyCell>/home</TableBodyCell>
-            <TableBodyCell>01/01/2001 00:00</TableBodyCell>
-            <TableBodyCell>
-                <ButtonGroup>
-                    <Button href="/">
-                        <CloudArrowUpSolid class="w-4 h-4 me-2"/>
-                        Upload
-                    </Button>
-                    <Button href="/">
-                        <EditSolid class="w-4 h-4 me-2"/>
-                        Edit
-                    </Button>
-                </ButtonGroup>
-            </TableBodyCell>
-        </TableBodyRow>
+        {#each shareData as data}
+            <TableBodyRow>
+                <TableBodyCell>{data.name}</TableBodyCell>
+                <TableBodyCell>{data.path}</TableBodyCell>
+                <TableBodyCell>{data.lastUploaded}</TableBodyCell>
+                <TableBodyCell>
+                    <ButtonGroup>
+                        <Button href="/">
+                            <CloudArrowUpSolid class="w-4 h-4 me-2"/>
+                            Upload
+                        </Button>
+                        <Button href="/shares/{data.id}/edit">
+                            <EditSolid class="w-4 h-4 me-2"/>
+                            Edit
+                        </Button>
+                    </ButtonGroup>
+                </TableBodyCell>
+            </TableBodyRow>
+        {/each}
         <TableBodyRow>
             <TableBodyCell></TableBodyCell>
             <TableBodyCell></TableBodyCell>
