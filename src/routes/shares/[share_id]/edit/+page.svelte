58880eae97a01dcf51b6099beed9952e67b5629d
philipp-schuetz <mail@philippschuetz.com>
2024-06-13 15:47:56 +0200

    Update edit page

--- src/routes/shares/[share_id]/edit/+page.svelte
+++ src/routes/shares/[share_id]/edit/+page.svelte
@@ -1,47 +1,48 @@
 <script lang="ts">
-    import {page} from '$app/stores';
+    import {Input, Label, Helper, Button, Checkbox, A, MultiSelect, Fileupload} from "flowbite-svelte";
+    import {goto} from "$app/navigation";
+    import {page} from "$app/stores";
 
-    let shareId = $page.params.share_id;
+    const shareId = $page.params.share_id;
 
-    import {Input, Label, Helper, Button, Checkbox, A} from 'flowbite-svelte';
-    import {MultiSelect} from 'flowbite-svelte';
-
-    import {Fileupload} from 'flowbite-svelte';
-
-    let value;
-
-    let selected: string = [];
-    let countries = [
-        {value: 'google_drive', name: 'Google Drive'},
-        {value: 'one_drive', name: 'One Drive'},
-        {value: 'ftp', name: 'FTP Server'},
-        {value: 'aws', name: 'Amazon Webservices'},
-        {value: 'gcp', name: 'Google Cloud Platform'}
+    let selectedProviders: string[] = [];
+    const shareData = { // share data is fetched by the provided share id
+        name: "Test Name",
+        path: "/path/to/directory",
+        providers: ["ftp", "aws"]
+    }
+    let shareDataEdited = shareData
+    const providers = [
+        {value: "google_drive", name: "Google Drive"},
+        {value: "one_drive", name: "One Drive"},
+        {value: "ftp", name: "FTP Server"},
+        {value: "aws", name: "Amazon Webservices"},
+        {value: "gcp", name: "Google Cloud Platform"}
     ];
+    function reset() {
+        shareDataEdited = shareData
+    }
+    function submit() {
+        goto("/shares");
+        // TODO send data to backend
+    }
 </script>
 
 <form>
-
     <div class="mb-6">
         <Label for="first_name" class="mb-2">Share name</Label>
-        <Input type="text" id="share_name" placeholder="Documents" required/>
-    </div>
-    <div class="mb-6">
-        <Label for="last_name" class="mb-2">Path</Label>
-        <Input type="text" id="last_name" placeholder="/home/documents" required/>
-    </div>
-    <div class="mb-6">
-        <Label class="space-y-2 mb-2">
-            <span>Upload file</span>
-            <Fileupload bind:value/>
-        </Label>
-        <Label>File: {value}</Label>
+        <Input type="text" id="share_name" placeholder="Documents" bind:value={shareDataEdited.name} required/>
     </div>
     <div class="mb-6">
         <Label for="providers" class="mb-2">Providers</Label>
-        <MultiSelect items={countries} id="providers" bind:value={selected} size="md"/>
+        <MultiSelect items={providers} id="providers" bind:value={shareDataEdited.providers} disabled/>
     </div>
-    <Button type="submit">Add</Button>
+    <div class="mb-6">
+        <Label for="last_name" class="mb-2">Path</Label>
+        <Input type="text" id="last_name" placeholder="/home/documents" bind:value={shareDataEdited.path} required disabled/>
+    </div>
+    <Button type="submit" on:click={submit}>Edit</Button>
+    <Button on:click={reset}>Reset</Button>
 </form>
 
 
