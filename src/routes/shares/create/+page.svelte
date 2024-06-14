cbfc030f1b30d839fd0263c578a4c30920926517
philipp-schuetz <mail@philippschuetz.com>
2024-06-14 20:27:53 +0200

    Add directory selection to share creation

--- src/routes/shares/create/+page.svelte
+++ src/routes/shares/create/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    import {Input, Label, Helper, Button, Checkbox, A, MultiSelect, Fileupload} from 'flowbite-svelte';
+    import {Input, Label, Button, MultiSelect} from 'flowbite-svelte';
     import {goto} from "$app/navigation";
+    import {open} from '@tauri-apps/api/dialog';
 
-    let value;
-    let selected: string = [];
+    let selected: string[] = [];
     let providers = [
         {value: 'google_drive', name: 'Google Drive'},
         {value: 'one_drive', name: 'One Drive'},
@@ -11,25 +11,40 @@
         {value: 'aws', name: 'Amazon Webservices'},
         {value: 'gcp', name: 'Google Cloud Platform'}
     ];
+    let directory: string;
+
     function submit() {
         goto('/shares');
+    }
+
+    async function directoryDialog() {
+        const selected = await open({
+            directory: true,
+            multiple: false,
+        });
+
+        if (!Array.isArray(selected) && selected !== null) {
+            directory = selected;
+        }
+        return;
     }
 </script>
 
 <form>
     <div class="mb-6">
         <Label for="first_name" class="mb-2">Share name</Label>
-        <Input type="text" id="share_name" placeholder="Documents" required/>
+        <Input type="text" id="share_name" placeholder="" required/>
     </div>
     <div class="mb-6">
         <Label for="providers" class="mb-2">Providers</Label>
         <MultiSelect items={providers} id="providers" bind:value={selected} size="md"/>
     </div>
     <div class="mb-6">
-        <Label for="last_name" class="mb-2">Path</Label>
-        <Input type="text" id="last_name" placeholder="/home/documents" required/>
+        <Label for="last_name" class="mb-2">Directory</Label>
+        <Input type="text" id="last_name" placeholder="" bind:value={directory} readonly required/>
+        <Button on:click={directoryDialog}>Select Directory</Button>
     </div>
-    <Button type="submit" on:click={submit}>Create</Button>
+    <Button type="submit" on:submit={submit}>Create</Button>
 </form>
 
 
