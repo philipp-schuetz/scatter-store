6a1a34d3da47b80ccac4532c9436c5d2f7756857
philipp-schuetz <mail@philippschuetz.com>
2024-06-14 21:06:06 +0200

    Update shares details page

--- src/routes/shares/[share_id]/details/+page.svelte
+++ src/routes/shares/[share_id]/details/+page.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-    import {Input, Label, Helper, Button, Checkbox, A, MultiSelect, Fileupload} from "flowbite-svelte";
+    import {Input, Label, Button, MultiSelect} from "flowbite-svelte";
     import {goto} from "$app/navigation";
     import {page} from "$app/stores";
 
     const shareId = $page.params.share_id;
 
-    let selectedProviders: string[] = [];
     const shareData = { // share data is fetched by the provided share id
         name: "Test Name",
         path: "/path/to/directory",
         providers: ["ftp", "aws"]
     }
-    let shareDataEdited = shareData
+    let shareDataEdited = {...shareData};
     const providers = [
         {value: "google_drive", name: "Google Drive"},
         {value: "one_drive", name: "One Drive"},
@@ -19,30 +18,35 @@
         {value: "aws", name: "Amazon Webservices"},
         {value: "gcp", name: "Google Cloud Platform"}
     ];
-    function reset() {
+    function resetValue() {
         shareDataEdited = shareData
     }
     function submit() {
         goto("/shares");
         // TODO send data to backend
     }
+    function deleteShare() {
+        goto("/shares");
+        // TODO delete db entry; also delete remote data?
+    }
 </script>
 
 <form>
     <div class="mb-6">
-        <Label for="first_name" class="mb-2">Share name</Label>
-        <Input type="text" id="share_name" placeholder="Documents" bind:value={shareDataEdited.name} required/>
+        <Label for="name" class="mb-2">Share name</Label>
+        <Input type="text" id="name" placeholder="" bind:value={shareDataEdited.name} required/>
     </div>
     <div class="mb-6">
         <Label for="providers" class="mb-2">Providers</Label>
         <MultiSelect items={providers} id="providers" bind:value={shareDataEdited.providers} disabled/>
     </div>
     <div class="mb-6">
-        <Label for="last_name" class="mb-2">Path</Label>
-        <Input type="text" id="last_name" placeholder="/home/documents" bind:value={shareDataEdited.path} required disabled/>
+        <Label for="directory" class="mb-2">Path</Label>
+        <Input type="text" id="directory" placeholder="" bind:value={shareDataEdited.path} disabled/>
     </div>
-    <Button type="submit" on:click={submit}>Edit</Button>
-    <Button on:click={reset}>Reset</Button>
+    <Button type="submit" on:submit={submit}>Edit</Button>
+    <Button on:click={resetValue}>Reset</Button>
+    <Button on:click={deleteShare}>Delete</Button>
 </form>
 
 
