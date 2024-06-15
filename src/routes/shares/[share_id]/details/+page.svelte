3535a90abb1f96f75babb9cc651c558a0098b94f
philipp-schuetz <mail@philippschuetz.com>
2024-06-15 11:13:32 +0200

    Remove edit form for shares

--- src/routes/shares/[share_id]/details/+page.svelte
+++ src/routes/shares/[share_id]/details/+page.svelte
@@ -10,7 +10,6 @@
         path: "/path/to/directory",
         providers: ["ftp", "aws"]
     }
-    let shareDataEdited = {...shareData};
     const providers = [
         {value: "google_drive", name: "Google Drive"},
         {value: "one_drive", name: "One Drive"},
@@ -18,36 +17,21 @@
         {value: "aws", name: "Amazon Webservices"},
         {value: "gcp", name: "Google Cloud Platform"}
     ];
-    function resetValue() {
-        shareDataEdited = shareData
-    }
-    function submit() {
+
+    function back() {
         goto("/shares");
-        // TODO send data to backend
     }
+
     function deleteShare() {
         goto("/shares");
         // TODO delete db entry; also delete remote data?
     }
 </script>
 
-<form>
-    <div class="mb-6">
-        <Label for="name" class="mb-2">Share name</Label>
-        <Input type="text" id="name" placeholder="" bind:value={shareDataEdited.name} required/>
-    </div>
-    <div class="mb-6">
-        <Label for="providers" class="mb-2">Providers</Label>
-        <MultiSelect items={providers} id="providers" bind:value={shareDataEdited.providers} disabled/>
-    </div>
-    <div class="mb-6">
-        <Label for="directory" class="mb-2">Path</Label>
-        <Input type="text" id="directory" placeholder="" bind:value={shareDataEdited.path} disabled/>
-    </div>
-    <Button type="submit" on:submit={submit}>Edit</Button>
-    <Button on:click={resetValue}>Reset</Button>
-    <Button on:click={deleteShare}>Delete</Button>
-</form>
+Name: {shareData.name}<br>
+Providers: {shareData.providers}<br>
+Directory: {shareData.path}<br>
 
-
+<Button on:click={back}>Back</Button>
+<Button on:click={deleteShare}>Delete</Button>
 
