58243c3d39b8b1c19ad07af6cb4c7e0d34cc2154
philipp-schuetz <mail@philippschuetz.com>
2024-06-29 12:11:42 +0200

    Add table to providers page

--- src/routes/providers/+page.svelte
+++ src/routes/providers/+page.svelte
@@ -1,6 +1,69 @@
 <script lang="ts">
+    import {
+        Table,
+        TableBody,
+        TableBodyCell,
+        TableBodyRow,
+        TableHead,
+        TableHeadCell,
+        ButtonGroup,
+        Button
+    } from 'flowbite-svelte';
+    import {CloudArrowUpOutline, ListOutline, CirclePlusSolid} from 'flowbite-svelte-icons';
+
+    const providerData: Provider[] = [
+        {
+            id: "1234ger34g43QW",
+            name: "ftp",
+            displayName: "FTP Server 1",
+        },
+        {
+            id: "pq93gh8q384gnf",
+            name: "google_drive",
+            displayName: "Google Drive Main",
+        },
+        {
+            id: "qo34bg89qqg",
+            name: "gcp",
+            displayName: "Google Cloud Platform",
+        },
+    ]
+
 </script>
 
-<div class="container">
-    Providers
-</div>
+<Table striped={true} shadow>
+    <TableHead>
+        <TableHeadCell>Name</TableHeadCell>
+        <TableHeadCell>
+            <span class="sr-only">Actions</span>
+        </TableHeadCell>
+    </TableHead>
+    <TableBody tableBodyClass="divide-y">
+        {#each providerData as data}
+            <TableBodyRow>
+                <TableBodyCell>{data.displayName}</TableBodyCell>
+                <TableBodyCell>
+                    <ButtonGroup>
+                        <Button href="/providers/{data.id}/details">
+                            <ListOutline class="w-4 h-4 me-2"/>
+                            Details
+                        </Button>
+                    </ButtonGroup>
+                </TableBodyCell>
+            </TableBodyRow>
+        {/each}
+        <TableBodyRow>
+            <TableBodyCell></TableBodyCell>
+            <TableBodyCell></TableBodyCell>
+            <TableBodyCell></TableBodyCell>
+            <TableBodyCell>
+                <ButtonGroup>
+                    <Button href="/providers/create">
+                        <CirclePlusSolid class="w-4 h-4 me-2"/>
+                        Create a provider
+                    </Button>
+                </ButtonGroup>
+            </TableBodyCell>
+        </TableBodyRow>
+    </TableBody>
+</Table>
